cc3ea7ec0b1a97589700a110c235b0cdd0026ba9
maulidafk1 <maulidafk1@gmail.com>
2026-05-22 14:16:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,5 @@ st.write(
 )
 import streamlit as st
 
-st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
-st.subheader("This is a subheader with a divider", divider="gray")
-st.subheader("These subheaders have rotating dividers", divider=True)
-st.subheader("One", divider=True)
-st.subheader("Two", divider=True)
-st.subheader("Three", divider=True)
-st.subheader("Four", divider=True)
+st.subheader("_Welcome_ to the Aplikasi Maul :pink[cool] :sunglasses:")
+
